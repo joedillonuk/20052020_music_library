061a830a1c0b82c0c816b7ba600ee12765043ab6
joedillonuk <joe.dillon.uk@gmail.com>
2020-05-20 17:39:25 +0100

    All updated, additonal albums added.

--- db/console.rb
+++ db/console.rb
@@ -7,7 +7,14 @@ Artist.delete_all()
 
 artist1 = Artist.new({'name' => 'Tom Waits'})
 artist1.save()
-
+artist2 = Artist.new({'name' => 'Nick Cave'})
+artist2.save()
+artist3 = Artist.new({'name' => 'Sheer Mag'})
+artist3.save()
+artist4 = Artist.new({'name' => 'PJ Harvey'})
+artist4.save()
+artist5 = Artist.new({'name' => 'Manowar'})
+artist5.save()
 
 album1 = Album.new({
 'title' => 'Nighthawks At The Diner',
@@ -24,12 +31,74 @@ album2 = Album.new({
   })
 album2.save()
 
+album3 = Album.new({
+'title' => 'Bad As Me',
+'genre' => 'Rock',
+'year' => 2011,
+'artist_id' => artist1.id
+  })
+album3.save()
+
+
+album4 = Album.new({
+'title' => 'Murder Ballads',
+'genre' => 'Rock',
+'year' => 1996,
+'artist_id' => artist2.id
+  })
+album4.save()
+
+album5 = Album.new({
+'title' => 'Dig, Lazarus, Dig!!!',
+'genre' => 'Rock',
+'year' => 2008,
+'artist_id' => artist2.id
+  })
+album5.save()
+
+album6 = Album.new({
+'title' => 'Need To Feel Your Love',
+'genre' => 'Punk',
+'year' => 2017,
+'artist_id' => artist3.id
+  })
+album6.save()
+
+album7 = Album.new({
+'title' => 'Stories from the City, Stories from the Sea',
+'genre' => 'Rock',
+'year' => 2020,
+'artist_id' => artist4.id
+  })
+album7.save()
+
+album8 = Album.new({
+'title' => 'Fighting The World',
+'genre' => 'Metal',
+'year' => 2087,
+'artist_id' => artist5.id
+  })
+album8.save()
 
 p "The artist of #{album1.title} is #{album1.artist().name}."
+
+# Updating album2 from Rain Dogs to Swordfishtrombones.
 p album2
 album2.title = 'Swordfishtrombones'
 album2.year = 1983
 album2.update()
 p album2
+
+# Listing all albums by artist1 (Tom Waits)
+p artist1.albums()
+
+# Updating artist2's name
+p artist2.name
+artist2.name = 'Nick Cave & The Bad Seeds'
+artist2.update()
+p artist2.name
+
+
+
 binding.pry
 nil
--- models/artist.rb
+++ models/artist.rb
@@ -29,25 +29,46 @@ class Artist
     end
 
     def self.delete_all()
-        sql = "DELETE FROM artists"
-        SqlRunner.run(sql)
-      end
+      sql = "DELETE FROM artists"
+      SqlRunner.run(sql)
+    end
 
-      def self.all()
-            sql = "SELECT * FROM artists"
-            artist = SqlRunner.run(sql)
-            return artists.map { |artist| Artist.new(artist)}
-          end
+    def self.all()
+      sql = "SELECT * FROM artists"
+      artist = SqlRunner.run(sql)
+      return artists.map { |artist| Artist.new(artist)}
+    end
 
-          # List all albums by an artist
+    # List all albums by an artist
+    def albums()
+      sql = "SELECT * FROM albums WHERE artist_id = $1"
+      values = [@id]
+      results = SqlRunner.run(sql, values)
+      albums = results.map{|album| Album.new(album)}
+      return albums
+    end
 
+    # Update Artist/album
+    def update()
+      sql = "UPDATE artists SET name = $1 WHERE id = $2"
+      values = [@name, @id]
+      SqlRunner.run(sql, values)
+    end
 
+    # Delete specific artist/albums
+    def delete() #delete by id
+      sql = "DELETE FROM artists WHERE id = $1"
+      values = [@id]
+      SqlRunner.run(sql, values)
+    end
 
-          # Get the artist for a particular albums
-          # Update Artist/album
-          # Delete specific artist/albums
-          # Find Artist/Album by ID
+    # Find Artist/Album by ID
+    def Artist.find_by_id(id)
+      sql = "SELECT * FROM artists WHERE id = $1"
+      values = [id]
+      result = SqlRunner.run(sql, values)
+      return Artist.new(result[0])
+    end
 
-
-
+    
   end
